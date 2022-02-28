99ab61a05832f7c67eb036558123f6a661643a33
Paakofi62 <55417163+Paakofi62@users.noreply.github.com>
2022-02-28 14:29:27 -0500

    Add files via upload

---
+++ model.py
@@ -0,0 +1,4 @@
+#from django.db import model
+
+#storage = S3Storage(aws_s3_bucket_name=settings.DOCUMENTS_BUCKET)
+#upload_location = 'recordings/'
---
+++ models.py
@@ -0,0 +1,9 @@
+#from django.db import models
+#DEFAULT_FILE_STORAGE = 'django.db.backends.sqlite3'
+#storage = OneNineStorage(location=settings.MEDIA_ROOT)
+#upload_location = 'recordings/'
+#zip_file = models.FileField( upload_to = upload_location, storage=storage, null=True) 
+#if settings.DEBUG:
+ # storage = FileSystemStorage(location=' ')
+  #upload_location = ''
+# Create your models here.
